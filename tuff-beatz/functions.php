40c5b6a6dce04b42bc26aa8d5571be37e0a23894
Emmanuel Tuffet <dontuffet@gmail.com>
2026-08-31 16:56:51 -0400

    Load Studio OS project file manager

--- tuff-beatz/functions.php
+++ tuff-beatz/functions.php
@@ -79,3 +79,4 @@ require_once get_template_directory() . '/inc/project-invoicing.php';
 require_once get_template_directory() . '/inc/portal-hubs.php';
 require_once get_template_directory() . '/inc/project-notifications.php';
 require_once get_template_directory() . '/inc/client-crm.php';
+require_once get_template_directory() . '/inc/project-file-manager.php';
