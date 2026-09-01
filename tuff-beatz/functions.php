b3366f7d00af4e58b610e82d67eeebc93fad917e
Emmanuel Tuffet <dontuffet@gmail.com>
2026-08-31 20:41:26 -0400

    Load Studio Conversation Center

--- tuff-beatz/functions.php
+++ tuff-beatz/functions.php
@@ -80,5 +80,6 @@ require_once get_template_directory() . '/inc/portal-hubs.php';
 require_once get_template_directory() . '/inc/project-notifications.php';
 require_once get_template_directory() . '/inc/client-crm.php';
 require_once get_template_directory() . '/inc/project-file-manager.php';
+require_once get_template_directory() . '/inc/project-conversation.php';
 require_once get_template_directory() . '/inc/workflow-automation.php';
 require_once get_template_directory() . '/inc/producer-audio-console.php';
